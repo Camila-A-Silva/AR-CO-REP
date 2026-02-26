3dd57014a66f8c7bb6819ec4a82da367085d8da0
Camila Silva <camila.a.silva19@aluno.senai.br>
2026-02-26 11:24:35 -0300

    códificando o main, os botões para ir para a página projetos estão prontos

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// 1. Lógica do Meno Principal (Index, obter os elementos)
+const btnP1 = document.querySelector('#inputP1');
+const btnP2 = document.querySelector('#inputP2');
+const btnP3 = document.querySelector('#inputP3');
+
+// 2. Método addEventListener com ma função anônima
+//  função de click para chamar a página projeto01no index
+btnP1.addEventListener('click',function(){
+    window.location.href = './pages/projeto01.html'
+});
+
+btnP2.addEventListener('click',function(){
+    window.location.href = './pages/projeto02.html'
+});
+
+btnP3.addEventListener('click',function(){
+    window.location.href = './pages/projeto03.html'
+});
+
+
